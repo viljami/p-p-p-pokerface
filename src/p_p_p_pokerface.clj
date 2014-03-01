3bd14c5f259d93659c2e5ba5228ea532abd900b5
viljami <viljami@zeebox.com>
2014-03-01 17:07:07 +0000

    Pairs, three of kind and four of kind working.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -2,26 +2,25 @@
 
 (def replacements {\T 10 \J 11 \Q 12 \K 13 \A 14 })
 
-(defn rank [card]
-  (let [[fst _] card]
-    (if (Character/isDigit fst)
-      (Integer/valueOf (str fst))
-      (replacements fst)
-      )
-    )
-  )
+(defn rank [[fst _]]
+  (if (Character/isDigit fst)
+    (Integer/valueOf (str fst))
+    (replacements fst)))
 
-(defn suit [card]
-  (let [[_ snd] card] (str snd)))
+(defn suit [[_ snd]] (str snd))
 
+(defn amounts [hand] (vals (frequencies (map rank hand))))
+(defn highest [hand] (apply max (amounts hand)))
 (defn pair? [hand]
-  nil)
+  (== 2 (highest hand)))
+
+(def pair-hand ["2H" "2S" "4C" "5C" "7D"])
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== 3 (highest hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== 4 (highest hand)))
 
 (defn flush? [hand]
   nil)
@@ -41,4 +40,19 @@
 (defn value [hand]
   nil)
 
-(suit "5H")
+
+(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
+
+
+(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
+(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
+(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
+
+(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
+(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
+(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
+(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
+(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
+(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
+(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
+(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
